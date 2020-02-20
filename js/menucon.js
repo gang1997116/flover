c12411f598032c66d49bc98ae0482910042a26a7
Kelly Zhong <gang1997116@users.noreply.github.com>
2020-02-20 13:43:50 +0000

    新增魔方页商品详情页,更新导航.
    新增魔方页商品详情页,更新导航(包括购物车和菜单).

---
+++ js/menucon.js
@@ -0,0 +1,14 @@
+
+function doHide(){
+    var oDiv = document.getElementById("nav1");
+    // var oButton = document.getElementById("menu");
+
+    if (oDiv.style.display == "none"){
+        oDiv.style.display = "flex";
+
+    }else {
+        oDiv.style.display = "none";
+
+    }
+
+}
