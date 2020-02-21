1ae14e2e0c57126274eaaffebb6a227b08aa945c
Kelly Zhong <gang1997116@users.noreply.github.com>
2020-02-21 23:07:24 +0000

    更改字体, 放大魔方页、购物车页,增加按钮功能,解决导航bug
    更改字体, 放大魔方页、购物车页,增加按钮add to basket功能,解决导航bug(第一次按不出的bug)

--- js/menucon.js
+++ js/menucon.js
@@ -1,20 +1,15 @@
 
-window.onload=function(){
-    var oDiv = document.getElementById("nav1");
-    oDiv.style.display = "none";
-    var basket = document.getElementById("basket");
-    basket.style.display = "none";
-}
+
 function doHide(){
     var oDiv = document.getElementById("nav1");
     // var oButton = document.getElementById("menu");
 
-    if (oDiv.style.display =="none")
+    if (oDiv.style.display =="flex")
     {
-        oDiv.style.display = "flex";
+        oDiv.style.display = "none";
 
     }else {
-        oDiv.style.display = "none";
+        oDiv.style.display = "flex";
 
     }
 
@@ -24,12 +19,12 @@ function basketshow() {
     var oDiv = document.getElementById("basket");
     // var oButton = document.getElementById("menu");
 
-    if (oDiv.style.display =="none")
+    if (oDiv.style.display =="block")
     {
-        oDiv.style.display = "block";
+        oDiv.style.display = "none";
 
     }else {
-        oDiv.style.display = "none";
+        oDiv.style.display = "block";
 
     }
 }
